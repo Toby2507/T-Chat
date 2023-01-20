72c80fffbccb9d42dfb72d81d83dd0025e260bdb
Toby2507 <tswagsurboi@gmail.com>
2023-01-20 14:23:23 +0100

    Minor UI change

--- client/src/components/layouts/Layout.tsx
+++ client/src/components/layouts/Layout.tsx
@@ -2,7 +2,7 @@ import { Outlet } from 'react-router-dom';
 
 const Layout = () => {
     return (
-        <main className="font-poppins setDeviceHeight w-full bg-black bg-fixed">
+        <main className="font-poppins bg-black bg-fixed overflow-hidden" style={{ "height": `${window.innerHeight}px`, "width": `${window.innerWidth}px` }}>
             <Outlet />
         </main>
     );
