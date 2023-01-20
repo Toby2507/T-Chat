834245b3ee76269837a51080f34a3538065552c9
Toby2507 <tswagsurboi@gmail.com>
2023-01-20 16:04:33 +0100

    Minor UI changes to the chat container and chat layout components

--- client/src/features/chats/ChatContainer.tsx
+++ client/src/features/chats/ChatContainer.tsx
@@ -64,7 +64,7 @@ const ChatContainer = () => {
 
   useEffect(() => { scrollRef.current?.scrollIntoView({ behavior: "smooth" }); }, [messages]);
   return (
-    <>
+    <div className="relative w-full h-full">
       <section className="w-full h-full border-l border-mainGray grid grid-rows-[auto_1fr_auto]">
         <div className="relative px-4 py-1 flex gap-2 items-center bg-mainGray">
           <button className="lg:hidden" onClick={() => dispatch(toggleChatBox({ show: false, chat: { id: null, isGroup: false } }))}><IoIosArrowBack className='text-lg text-white' /></button>
@@ -100,7 +100,7 @@ const ChatContainer = () => {
         </>
       </section>
       {loading && <Loader />}
-    </>
+    </div>
   );
 };
 
